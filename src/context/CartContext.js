4e5a4c374e5cb3fbae7991497d298663e104c661
Manuel Oliva Velez <manuel.oliva.velez@gmail.com>
2021-08-31 22:23:11 -0300

    Actualice el componente CartContext

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,16 +1,14 @@
 import { createContext, useState } from "react";
-import { drinks } from "../components/drinks";
 
 const CartContext = createContext ([]);
 
 export const CartProvider = ({ children }) => {
 
-    const [ products, setProducts ] = useState(drinks);
     const [ cart, setCart ] = useState([]);
 
     return (
         <CartContext.Provider
-            value={{ products, setProducts, cart, setCart }}
+            value={{ cart }}
         >
             { children }
         </CartContext.Provider>
